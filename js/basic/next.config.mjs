da18dd68916d6a61cf96af12631f72aca082663e
ciaranshan1 <ciaran.shanahan@wpengine.com>
2024-01-16 15:47:12 +0000

    add require resolve

--- js/basic/next.config.mjs
+++ js/basic/next.config.mjs
@@ -1,16 +1,20 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
 const getAtlasCacheHandler = async (config = {}) => {
   // if (process.env.ATLAS_CACHE_HANDLER_ENABLED === undefined) {
   //     return { ...config };
   // }
 
   // const { CacheHandler } = await import("./.atlas/atlas-cache-handler.js");
-  // const { atlasCacheHandler } = await import('./.atlas/atlas-cache-handler.js');
+  // const require = createRequire(import.meta.url);
+  // const { atlasCacheHandler } = require.resolve('./.atlas/atlas-cache-handler.js');
 
 
   return {
     ...config,
     ...{
-      incrementalCacheHandlerPath: "./.atlas/atlas-cache-handler.js",
+      incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
       isrMemoryCacheSize: 0,
     },
   };
