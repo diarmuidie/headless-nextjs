4a9d120ae3308bc918c199d332dc451bcb69b524
Daragh Lowe <daragh.lowe@wpengine.com>
2024-02-28 17:29:55 +0000

    next 14.1.0 and node 18 with next.config.mjs

---
+++ js/basic/next.config.mjs
@@ -0,0 +1,80 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+import { withAtlasConfig } from "@wpengine/atlas-next"
+
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    // output: 'standalone',
+    async rewrites() {
+        return {
+            beforeFiles: [
+                // These rewrites are checked after headers/redirects
+                // and before all files including _next/public files which
+                // allows overriding page files
+                {
+                    source: '/this/:path*',
+                    destination: '/:one/:path*',
+                    has: [
+                        {
+                            type: 'header',
+                            key: 'one',
+                            value: '(?<one>.*)',
+                        },
+                    ],
+                },
+                // {
+                //   source: '/:first/:second',
+                //   destination: '/:first?second=:second',
+                // },
+            ],
+            afterFiles: [
+                // These rewrites are checked after pages/public files
+                // are checked but before dynamic routes
+                {
+                    source: '/after',
+                    destination: '/somewhere-else',
+                },
+            ],
+            fallback: [
+                // These rewrites are checked after both pages/public files
+                // and dynamic routes are checked
+                {
+                    source: '/fallback/:path*',
+                    destination: `https://my-old-site.com/:path*`,
+                },
+            ],
+        }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/bar/:path',
+                destination: '/:path',
+                permanent: true,
+            },
+        ]
+    },
+    // output: 'standalone',
+    async headers() {
+        return [
+            {
+                source: '/foo',
+                headers: [
+                    {
+                        key: 'x-foo',
+                        value: 'some-foo',
+                    },
+                    {
+                        key: 'x-bar',
+                        value: 'some-bar',
+                    },
+                ],
+            },
+        ]
+    },
+}
+
+export default withAtlasConfig(nextConfig)
