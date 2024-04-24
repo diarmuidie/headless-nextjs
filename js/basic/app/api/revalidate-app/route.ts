5ff1fced4c2b3c06d2ec6156143ae4bfb526ac57
ciaranshan1 <ciaran.shanahan@wpengine.com>
2024-04-24 17:12:02 +0100

    add better api route

--- js/basic/app/api/revalidate-app/route.ts
+++ js/basic/app/api/revalidate-app/route.ts
@@ -3,10 +3,14 @@ import { revalidatePath, revalidateTag } from 'next/cache';
 
 export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get('path') || '';
+  const type = request.nextUrl.searchParams.get('type') || '';
   if (path != '') {
-    if (path.includes('[') && path.includes(']')) {
+    if (type == 'page') {
       revalidatePath(path, 'page');
       console.log('revalidated path page', path);
+    } else if (type == 'layout') {
+      revalidatePath(path, 'layout');
+      console.log('revalidated path layout', path);
     } else {
       revalidatePath(path);
       console.log('revalidated path', path);
