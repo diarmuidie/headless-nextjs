89af47214f14715b4271ad83c45f55c74b7c625c
ciaranshan1 <ciaran.shanahan@wpengine.com>
2024-07-02 16:59:19 +0100

    add new revalidate route

---
+++ js/basic/pages/api/dextero.js
@@ -0,0 +1,41 @@
+export default async function handler(req, res) {
+    const {
+        body: { paths },
+        method,
+    } = req
+
+    if (req.headers.authorization !== `Bearer ${process.env.REVALIDATE_SECRET_KEY}`) {
+        return res.status(401).json({ message: 'Invalid token' })
+    }
+
+    if (method !== 'PUT') {
+        return res.status(405).json({ message: `Method ${method} Not Allowed` })
+    }
+
+    if (!paths) {
+        return res.status(412).json({ message: 'No paths' })
+    }
+
+    const correctPaths = paths.filter((path) => path.startsWith('/'))
+
+    try {
+        const revalidatePaths = correctPaths.map((path) => res.revalidate(
+            path,
+            { unstable_onlyGenerated: false }
+        ));
+
+        await Promise.all(revalidatePaths);
+
+        // Logging for debugging purposes only
+        console.log(`${new Date().toJSON()} - Paths revalidated: ${correctPaths.join(', ')}`)
+
+        return res.json({
+            revalidated: true,
+            message: `Paths revalidated: ${correctPaths.join(', ')}`
+        })
+
+    } catch (err) {
+
+        return res.status(500).json({ message: err.message })
+    }
+}
