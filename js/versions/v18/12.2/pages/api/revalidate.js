8edf56607ee1af144cbe5c45c21218ec1c605ab0
ciaranshan1 <ciaran.shanahan@wpengine.com>
2024-07-02 17:47:52 +0100

    update package and add api route

---
+++ js/versions/v18/12.2/pages/api/revalidate.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const page = req.query.page
+
+  if (!page) {
+    res.status(500).send('Page query param not found')
+  }
+
+  try {
+    await res.revalidate(req.query.page)
+    console.log('Revalidated', req.query.page)
+    return res.json({ revalidated: true })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send('Error revalidating')
+  }
+}
